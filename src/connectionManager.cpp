bc43cbcad6578403172e4906ed8bac26d5194570
aatran14 <tranandy012@gmail.com>
2026-03-11 14:40:43 -0400

    Phase 1 of Andy-TODO: Fix Loop Bug and Handshake send

--- src/connectionManager.cpp
+++ src/connectionManager.cpp
@@ -77,8 +77,25 @@ int connectionManager::connectToPeer(int port, const char* address, int id) {
         return -1;
     }
 
-    for(p = servinfo; p != NULL; p->ai_addr) { // TODO-Andy Fix: Bug in loop - should be p = p->ai_next
-        
+
+
+    // for(p = servinfo; p != NULL; p->ai_addr) { // TODO-Andy Fix: Bug in loop - should be p = p->ai_next
+    /*
+    1. p->ai_addr is not doing anything. It reads a value and throws it out, which means p is not able to change. It can't.
+    2. As a result, the loop is either working on the first try, or it retries the same address forever. This is not what we want.
+
+    2.1. The intended behavior:
+    -> Try the first address
+    -> If connect fails then close the socket and move to the next address
+    -> If connect ever success, then break out
+    -> Repeat until one works or you run out of addresses.
+    This loop is for one peer only, but every peer is going to be doing this. This project, we are simplified and constrained to 6 peers.
+    A Full Mesh would be O(n^2) for connections since A-B is the same as B-A. 6peers CHOOSE 2peers = 15 connections
+
+    */
+
+    // Fix-Andy: Change p->ai_addr with p = p->ai_next
+    for(p = servinfo; p != NULL; p = p->ai_next){
         if((client_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             std::cout << "next client socket" << std::endl;
             continue;
@@ -118,10 +135,35 @@ int connectionManager::exchange(int fd, int id) {
     memset(handshake, 0, sizeof(handshake));
 // TODO-Andy: Peer ID should be 4-byte binary (network byte order), not ASCII string
     memcpy(handshake, "P2PFILESHARINGPROJ", 18);
-    memcpy(handshake + 18, "0000000000", 10);
-    std::string insertID = std::to_string(id);
-    memcpy(handshake + 28, insertID.c_str(), 4);
+    // memcpy(handshake + 18, "0000000000", 10);
 
+    /* There is an issue here by counterexample. 
+    If I have an id = 1001, this copies ASCII '1', '0', '0', '1' (bytes: 49, 48, 48, 49)
+
+    But Page 1 of specs/project description-v1.pdf says "4-byte integer representation" which means we want this in binary
+    1001 in binary (padded to 32 bits) is 0x000003E9.
+    In networks byte order, we use big-endian, so the representation is below
+    0x[00][00][03][E9]:
+    byte 0 = 0x00
+    byte 1 = 0x00
+    byte 2 = 0x03
+    byte 3 : 0xE9
+
+    Solving this means the Handshake Send is done
+    
+    */ 
+    
+    /*
+    Removed-Andy 
+    std::string insertID = std::to_string(id); 
+    memcpy(handshake + 28, insertID.c_str(), 4);
+    */
+    
+    //Replaced-Andy
+    uint32_t netId = htonl(id); // "host to network long" converts a 32-bit integer to big-endian. the arugment (id) is the 32-bit binary number. Keeps it as binary, but just fixes the byte order.
+    memcpy(handshake + 28, &netId, 4);
+
+    
     std::cout << "about to send handshake" << std::endl;
 
     if(send(fd, handshake, sizeof(handshake), 0) == -1) { //or 32 ig
@@ -130,8 +172,7 @@ int connectionManager::exchange(int fd, int id) {
     }
 
     std::cout << "handshake sent, waiting for recv..." << std::endl;
-// TODO-Andy: Validate received handshake - check header is "P2PFILESHARINGPROJ" and
-  verify peer ID
+// TODO-Andy: Validate received handshake - check header is "P2PFILESHARINGPROJ" and verify peer ID
     if((numberOfBytes = recv(fd, buffer, 32, 0)) == -1) {
         std::cout << "recv error" << std::endl;
         return -1;
