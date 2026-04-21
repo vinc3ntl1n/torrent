8d5e98b1ea4edfe5246f92176f759953523889a0
Vincent-Lin-UF <vincentlin1617@gmail.com>
2026-04-20 23:32:41 -0400

    Fixed main.cpp wiring and updated Makeefile with all the current source files

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,50 @@
 #include "connectionManager.h"
 #include "peerState.h"
 #include "fileManager.h"
+#include "logger.h"
 
 #include <iostream>
 #include <vector>
 #include <thread>
 #include <string>
-#include <bitset>
 #include <string.h>
-#include <string>
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
     commonFile commonConfig = returnCommon("config/unzipped/project_config_file_small/project_config_file_small/Common.cfg");
     std::vector<peerInfo> peers = returnPeerInfo("config/unzipped/project_config_file_small/project_config_file_small/PeerInfo.cfg");
-    //printCommon(commonConfig);
-    //printPeerInfo(peers);
 
-    // connectionManager test1;
-    // std::string option = argv[1];
-    // if(option == "1") {
-    //     test1.startServer(6000, 1001);
-    // }
-    // else if (option == "0") {
-    //     test1.connectToPeer(6000, "127.0.0.1", 1002);
-    // }
-    
-    // 1. get your peer ID from command line
     int myID = std::stoi(argv[1]);
 
-
-    peerInfo myInfo; //static information from config file
-    
+    peerInfo myInfo;
     for (const auto& peer: peers) {
         if (peer.peerID == myID) {
-            myInfo = peer; 
-        } 
+            myInfo = peer;
+        }
     }
-    //calculate how many pieces there are (weird ceiling division i found online)
+
     int totalPieces = (commonConfig.FileSize + commonConfig.PieceSize - 1) / commonConfig.PieceSize;
 
-//live runtime state of this peer
-    PeerState myState(peerInfo myInfo, int totalPieces);
+    PeerState myState(myInfo, totalPieces);
+    FileManager myFileManager(myID, commonConfig.FileName, commonConfig.FileSize, commonConfig.PieceSize, myInfo.hasFile);
+    Logger myLogger(myID);
+    connectionManager connMgr(&myState, &myFileManager, &myLogger, (int)peers.size(), myID);
 
+    std::thread serverThread(&connectionManager::startServer, &connMgr, myInfo.port, myID);
 
+    std::thread unchokThread(&connectionManager::unchokingTimer, &connMgr,
+        commonConfig.UnchokingInterval, commonConfig.NumberOfPreferredNeighbors);
+    std::thread optThread(&connectionManager::optimisticTimer, &connMgr,
+        commonConfig.OptimisticUnchokingInterval);
+    unchokThread.detach();
+    optThread.detach();
 
+    for (const auto& peer : peers) {
+        if (peer.peerID == myID) break;
+        connMgr.connectToPeer(peer.port, peer.hostName.c_str(), myID);
+    }
+
+    serverThread.join();
 
     return 0;
 }
